38d76338dff27a98d3d5976edb650c27983ff617
briannelson95 <71141646+briannelson95@users.noreply.github.com>
2022-04-17 17:43:36 -0400

    mega menu

--- tournament/components/Nav.js
+++ tournament/components/Nav.js
@@ -1,13 +1,20 @@
-import { Disclosure } from '@headlessui/react'
+import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { useRouter } from 'next/router'
-import { IoMenu, IoClose, IoLogoFacebook, IoLogoTwitter, IoLogoInstagram } from 'react-icons/io5'
+import { IoMenu, IoClose, IoLogoFacebook, IoLogoTwitter, IoLogoInstagram, IoChevronDown, IoChevronUp } from 'react-icons/io5'
 import styles from '../styles/Navbar.module.css'
+import { useState, Fragment } from 'react'
 
 const navigation = [
     {name: 'About', href: '/about'},
     {name: 'Tournament', href: '/tournament'},
     {name: 'Contact', href: '/contact'},
 ];
+
+const subMenu = [
+    {name: 'Rules', href: '/rules'},
+    {name: 'Teams', href: '/teams'},
+    {name: 'Schedule', href: '/schedule'},
+]
 
 const socials = [
     {icon: IoLogoFacebook, href: '/'},
@@ -17,6 +24,7 @@ const socials = [
 
 const Navbar = () => {
     const router = useRouter();
+    const [isShowing, setIsShowing] = useState(false)
 
     return (
         <Disclosure as='nav' className={styles.nav} >
@@ -35,9 +43,32 @@ const Navbar = () => {
                     </div>
                     <div className={styles.navList}>
                         <h1>Name/Logo</h1>
-                        {navigation.map((item, index) => (
-                            <div className={styles.navItem} key={index} onClick={() => router.push(`${item.href}`)}>{item.name}</div>
-                        ))}
+                        <div className={styles.navItem} onClick={navigation[0].href}>
+                            {navigation[0].name}
+                        </div>
+                        <Disclosure as='subNav' className={styles.subNav}>
+                            {({open}) => (
+                                <>
+                                    <Disclosure.Button className={styles.button}>
+                                        {open ? (
+                                            <div className={styles.navItem}>{navigation[1].name} <IoChevronUp size={15} className={styles.navItemIcon} /></div>
+                                        ) : (
+                                            <div className={styles.navItem}>{navigation[1].name} <IoChevronDown size={15} /></div>
+                                        )}
+                                    </Disclosure.Button>
+                                    <Disclosure.Panel className={styles.megaMenu} style={{position: 'absolute'}}>
+                                        <div className={styles.megaMenuItems}>
+                                            <div>{subMenu[0].name}</div>
+                                            <div>{subMenu[1].name}</div>
+                                            <div>{subMenu[2].name}</div>
+                                        </div>
+                                    </Disclosure.Panel>
+                                </>
+                            )}
+                        </Disclosure>
+                        <div className={styles.navItem} onClick={navigation[2].href}>
+                            {navigation[2].name}
+                        </div>
                         <div className={`${styles.navItem} ${styles.social}`}>
                             <IoLogoFacebook size={30} />
                         </div>
